32e68cb28e2d9572459fd0baa5e625845c73dee8
karanthakurr07 <karant081623@gmail.com>
2026-05-23 20:59:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,5 @@ def index():
 
 if __name__ == "__main__":
     print("🎬 Movie Recommender running at http://127.0.0.1:5000")
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
